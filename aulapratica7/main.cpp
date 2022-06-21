c50cadd039458babb02751f6530ec4a6229d0b16
jonhjonhb <augustosales2010@gmail.com>
2022-06-21 20:19:18 -0300

    inico pa 7

--- aulapratica7/main.cpp
+++ aulapratica7/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+void buscaPalavras();
+
 int main() {
-  
+    
   return 0;
+}
+
+void buscaPalavras(std::string &texto, std::string palavra){
+  int j = 1;
+  if (texto.substr(texto.size() - j, 1) == " "){
+    texto = texto.substr(0, texto.size() - j);
+  } 
+  for (int i = 0; i < 100; i++){
+    palavra = "";
+    for (j = 1; texto.substr(texto.size() - j, 1) != " " ; j++){}
+    palavra = texto.substr(texto.size() - j + 1);
+    texto = texto.substr(0, texto.size() - j);
+  }
 }
