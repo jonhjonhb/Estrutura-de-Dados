c71f1ebe2e3df8cc55654b6cbc6b35f656f302a6
jonhjonhb <augustosales2010@gmail.com>
2022-06-25 13:40:34 -0300

    add pratica7

--- aulapratica7/main.cpp
+++ aulapratica7/main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <string>
 
-void buscaPalavras();
-
-int main() {
-    
-  return 0;
-}
+using namespace std;
 
 void buscaPalavras(std::string &texto, std::string palavra){
-  int j = 1;
-  if (texto.substr(texto.size() - j, 1) == " "){
-    texto = texto.substr(0, texto.size() - j);
-  } 
-  for (int i = 0; i < 100; i++){
-    palavra = "";
-    for (j = 1; texto.substr(texto.size() - j, 1) != " " ; j++){}
-    palavra = texto.substr(texto.size() - j + 1);
-    texto = texto.substr(0, texto.size() - j);
+  int i = 0, pos = 0; 
+  bool sim = false;
+  while (texto.substr(i, 1) == " ") i++;
+  for (i = 0; i <= texto.size(); i++){
+    if (texto.substr(i, 1) == " " || texto.size() == i) {
+      if (texto.substr(0, i) == palavra) {
+        if(sim) cout << " ";
+        cout << pos;
+        sim = true;
+      }
+      pos += (i + 1);
+      if (i == texto.size()) texto = texto.substr(i);
+      else texto = texto.substr(i+1);
+      i = 0;
+    } 
   }
+  if (sim){
+    cout << endl;
+  } else{
+    cout << -1 << endl;
+  }
+}
+
+int main() {
+  int qtdTextos = 0;
+  string texto, palavra;
+
+  cin >> qtdTextos;
+
+  for (int i = 0; i < qtdTextos; i++) {
+    std::getline(cin, texto);
+    if(texto == "") std::getline(cin, texto);
+    cin >> palavra;
+    buscaPalavras(texto, palavra);
+  }
+  return 0;
 }
