46ddb13d22b8dbac8b6d18623010ddf01999c4b9
jonhjonhb <augustosales2010@gmail.com>
2022-06-26 13:22:59 -0300

    novos metodos getters, setters e operator++

--- TP2/TP/include/Word.hpp
+++ TP2/TP/include/Word.hpp
@@ -8,9 +8,22 @@ class Word{
     Word():_word(""), _freq(0){}
     Word(std::string str, int freq):_word(str), _freq(freq){}
     ~Word();
+    std::string getWord(){return _word;};
+    int getFrequencia(){return _freq;};
+    void setWord(std::string str);
+    void setFrequencia(int number);
+    void operator++(){_freq++;};
   private:
     std::string _word;
     int _freq;
 };
+
+void Word::setWord(std::string str){
+  _word = str;
+}
+
+void Word::setFrequencia(int number){
+  _freq = number;
+}
 
 #endif
